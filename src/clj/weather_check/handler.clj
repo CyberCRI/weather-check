076f1da30b04ab863c2e4cf141cdbbb3ab0ba8c4
Jesse Himmelstein <jesse@heavenstone.net>
2016-06-10 22:31:44 +0200

    Replaced figwheel text

--- src/clj/weather_check/handler.clj
+++ src/clj/weather_check/handler.clj
@@ -21,11 +21,7 @@
 ;;; WEBSITE
 
 (def mount-target
-  [:div#app
-      [:h3 "ClojureScript has not been compiled!"]
-      [:p "please run "
-       [:b "lein figwheel"]
-       " in order to start the compiler"]])
+  [:div#app "Loading..."])
 
 (defn head []
   [:head
